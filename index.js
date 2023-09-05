7fa4d292a16f4c7e7382d0c8b12bdc26fcde2039
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-09-06 01:47:59 +0600

    update coffee added

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ async function run() {
       res.send(result);
     });
 
+    //Update Data
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
     //Post data
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
@@ -41,6 +49,27 @@ async function run() {
       console.log(newCoffee);
     });
 
+    //Updated data
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.taste,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
     //Delete Data
     app.delete("/coffee/:id", async (req, res) => {
       const id = req.params.id;
